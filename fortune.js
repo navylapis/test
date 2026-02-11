0f08351be61e50f02b094f69a1971b11cc145945
navylapis <tekkamaki_1412@yahoo.co.jp>
2026-02-11 17:40:00 +0900

    占い結果設定

--- fortune.js
+++ fortune.js
@@ -19,7 +19,7 @@ assessmentButton.addEventListener(
   // headerDivision の作成
   const headerDivision = document.createElement('div');
   headerDivision.setAttribute('class', 'card-header text-bg-primary');
-  headerDivision.innerText = '診断結果';
+  headerDivision.innerText = '占い結果';
 
   // bodyDivision の作成
   const bodyDivision = document.createElement('div');
@@ -49,7 +49,7 @@ assessmentButton.addEventListener(
     anchor.setAttribute('href', hrefValue);
     anchor.setAttribute('class', 'twitter-hashtag-button');
     anchor.setAttribute('data-text', result);
-    anchor.innerText = 'Tweet #あなたのいいところ';
+    anchor.innerText = 'Tweet #占い結果';
   
     tweetDivision.appendChild(anchor);
 
@@ -70,22 +70,18 @@ userNameInput.addEventListener(
 )
 
 const answers = [
-  '###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
-  '###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
-  '###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。',
-  '###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。',
-  '###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。',
-  '###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。',
-  '###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。',
-  '###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。',
-  '###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。',
-  '###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。',
-  '###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。',
-  '###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。',
-  '###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。',
-  '###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
-  '###userName###のいいところはそのすべてです。ありのままの###userName###自身がいいところなのです。',
-  '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
+  '###userName###の運勢は大吉です。ガチャを引いてみるといいかも。ラッキーカラーは黄緑色',
+  '###userName###の運勢は大吉です。勝負をするのは今かも。ラッキーカラーは赤色',
+  '###userName###の運勢は大吉です。思い立ったら行動してみては？ラッキーカラーは黄色',
+  '###userName###の運勢は吉です。無くしたものが見つかるかも。ラッキーカラーは紫色',
+  '###userName###の運勢は吉です。レアドロップが出やすいかも。ラッキーカラーは青色',
+  '###userName###の運勢は中吉です。欲しいものが割引されるかも。ラッキーカラーは緑色',
+  '###userName###の運勢は中吉です。ロード時間がちょっと早いかも。ラッキーカラーはピンク',
+  '###userName###の運勢は小吉です。お菓子の袋がすぐ開けれるかも。ラッキーカラーは橙色',
+  '###userName###の運勢は小吉です。醤油の袋がすぐに切れるかも。ラッキーカラーは茶色',
+  '###userName###の運勢は末吉です。割り箸が綺麗に割れるかも。ラッキーカラーはベージュ',
+  '###userName###の運勢は末吉です。ゴミ箱にゴミを外さないかも。ラッキーカラーは水色',
+  '###userName###の運勢は凶です。小指をどこかにぶつけるかも。ラッキーカラーは黒'
 ];
 
 /**
